06c3916718c7b971386f89273c24cd8e873ee7e1
solainfire <gabrieldalvai1@gmail.com>
2022-01-17 15:49:12 +0100

    aaarfghj

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,54 +1,154 @@
-/**
- * ALGO: ceci est une classe...
- * Vous verrez ça plus tard en détail avec Rémi, pour l'instant on n'a pas trop besoin de savoir à quoi ça sert.
- */
 class Tableau1 extends Phaser.Scene{
-    preload (){
-        this.load.image('carré', 'assets/carre.png');
-        this.load.image('cercle', 'assets/cercle.png');
-        this.load.image ('gauche', 'assets/carre.png');
-        this.load.image('droite','assets/carre.png');
+    preload(){
+        this.load.image("cercle","assets/cercle.png")
+        this.load.image("carre","assets/carre.png")
     }
-    create(){
-        this.hauteur = 500
-        this.largeur = 1000
-
-        this.haut = this.physics.add.image(0,0,'gauche').setOrigin(0,0);
-        this.haut.setDisplaySize(this.largeur,20);
-        this.haut.setDisplaySize(this.hauteur,20);
+    create() {
+        this.width = 1000;
+        this.height = 500;
+        /**
+         * Mur Haut
+         * @type {Phaser.Physics.Arcade.Sprite & {body: Phaser.Physics.Arcade.Body}}
+         */
+        this.haut = this.physics.add.sprite(0, 0, 'carre').setOrigin(0, 0);
+        this.haut.setDisplaySize(this.width, 20);
         this.haut.body.setAllowGravity(false);
-
-
-
-        this.haut = this.physics.add.image(0,0,'carré').setOrigin(0,0);
-        this.haut.setDisplaySize(this.largeur,20);
-        this.haut.body.setAllowGravity(false);
-        this.haut.setImmovable(true)
-
-        this.bas = this.physics.add.image(0,this.hauteur-20,'carré').setOrigin(0,0);
-        this.bas.setDisplaySize(this.largeur,20);
+        this.haut.setImmovable(true);
+        /**
+         * Mur Bas
+         * @type {Phaser.Physics.Arcade.Sprite & {body: Phaser.Physics.Arcade.Body}}
+         */
+        this.bas = this.physics.add.sprite(0, this.height - 20, 'carre').setOrigin(0, 0);
+        this.bas.setDisplaySize(this.width, 20);
         this.bas.body.setAllowGravity(false);
-        this.bas.setImmovable(true)
+        this.bas.setImmovable(true);
+        /**
+         * Raquette Gauche
+         * @type {Phaser.Physics.Arcade.Sprite & {body: Phaser.Physics.Arcade.Body}}
+         */
+        this.gauche = this.physics.add.sprite(40, 200, 'carre').setOrigin(0, 0);
+        this.gauche.setDisplaySize(20, 100);
+        this.gauche.body.setAllowGravity(false);
+        this.gauche.setImmovable(true);
+        this.gauche.setVelocityY(0);
+        /**
+         * Raquette Droite
+         * @type {Phaser.Physics.Arcade.Sprite & {body: Phaser.Physics.Arcade.Body}}
+         */
+        this.droite = this.physics.add.sprite(this.width - 40, 200, 'carre').setOrigin(0, 0);
+        this.droite.setDisplaySize(20, 100);
+        this.droite.body.setAllowGravity(false);
+        this.droite.setImmovable(true);
+        this.droite.setVelocityY(0);
+        /**
+         * Balle
+         * @type {Phaser.Physics.Arcade.Sprite & {body: Phaser.Physics.Arcade.Body}}
+         */
+        this.balle = this.physics.add.sprite(this.width / 2, this.height / 2, 'cercle').setOrigin(0, 0);
+        this.balle.setDisplaySize(20, 20);
+        this.balle.body.setBounce(1.1, 1.1);
+        this.balle.setVelocityX(100);
+        this.balle.setVelocityY(Phaser.Math.Between(0,0));
+        this.balle.setMaxVelocity(500);
+        /**
+         * Physics
+         */
+        let me=this;
 
-        this.balle = this.physics.add.image(this.largeur/2, 10,'cercle').setOrigin(0,0);
-        this.balle.setDisplaySize(20,20);
-        this.balle.body.setBounce(1.1,1.1)
-        this.balle.setVelocityX(Phaser.Math.Between(200,-200))
-        this.balle.setVelocityY(Phaser.Math.Between(200,-200))
-        this.balle.setVelocityX(500)
+        this.physics.add.collider(this.balle, this.bas);
+        this.physics.add.collider(this.balle, this.haut);
+
+        this.physics.add.collider(this.balle, this.gauche);
+        this.physics.add.collider(this.balle, this.droite, function(){
+            console.log("touche droit")
+            me.rebond(me.droite);
+        });
+
+        this.initKeyboard();
+    }
+    rebond(raquette){
+
+        console.log(raquette.y);
+        console.log(this.balle.y);
+        console.log(this.balle.y-raquette.y);
 
 
-        this.physics.add.collider(this.balle,this.bas);
-        this.physics.add.collider(this.balle,this.haut);
 
+    }
+
+    initKeyboard() {
+        let me=this;
+        this.input.keyboard.on('keyup', function(kevent) {
+            switch (kevent.keyCode) {
+                // initialisation de la touche en appuis X pour descendre la raquette gauche
+                case Phaser.Input.Keyboard.KeyCodes.X:
+                    me.gauche.setVelocityY(0)
+                    break;
+                // initialisation de la touche en appuis S pour Monter la raquette gauche
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    me.gauche.setVelocityY(0)
+                    break;
+                // initialisation de la touche en appuis N pour descendre la raquette Droite
+                case Phaser.Input.Keyboard.KeyCodes.N:
+                    me.droite.setVelocityY(0)
+                    break;
+                // initialisation de la touche en appuis J pour Monter la raquette Droite
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                    me.droite.setVelocityY(0)
+                    break;
+            }
+        })
+        this.input.keyboard.on('keydown', function (kevent) {
+            switch (kevent.keyCode) {
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                    if(me.droite.y < me.haut.y+20){
+                        me.droite.setVelocityY(0)
+                    }
+                    else{
+                        me.droite.setVelocityY(-200)
+                    }
+                    break;
+
+                case Phaser.Input.Keyboard.KeyCodes.N:
+                    if(me.droite.y > me.bas.y-100){
+                        me.droite.setVelocityY(0)
+                    }else {
+                        me.droite.setVelocityY(200)
+                    }
+                    break;
+
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    if(me.gauche.y < me.haut.y+20){
+                        me.gauche.setVelocityY(0)
+                    }
+                    else{
+                        me.gauche.setVelocityY(-200)
+                    }
+                    break;
+
+                case Phaser.Input.Keyboard.KeyCodes.X:
+                    if(me.gauche.y > me.bas.y-100){
+                        me.gauche.setVelocityY(0)
+                    }else {
+                        me.gauche.setVelocityY(200)
+                    }
+
+                    break;
+            }
+        })
     }
     update(){
-
-        if(this.balle.x >  this.largeur){
-            this.balle.x =0
+        if(this.balle.x > this.width) {
+            this.balle.x=this.width/2
         }
-        if(this.y > this.hauteur){
-            this.balle.y = 0
+
+
+        if(this.balle.y<0){
+            this.balle.y=0
+        }
+
+        if(this.balle.y > this.height){
+            this.balle.y = this.height
         }
 
     }
